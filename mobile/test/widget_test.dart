1a62887ce820b2cf7a9dc8c7b6e0c2e3afc9b570
Angel Raimond <142851051+AngelRaimond@users.noreply.github.com>
2026-09-04 01:52:19 -0600

    Mobilepipe (#347)

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -4,11 +4,9 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Dummy widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: Text('Hello World'),
-      ),
-    ));
+    await tester.pumpWidget(
+      const MaterialApp(home: Scaffold(body: Text('Hello World'))),
+    );
 
     expect(find.text('Hello World'), findsOneWidget);
   });
