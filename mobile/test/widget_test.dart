28ae78b5800ecff4e1fe99e672fb520dc799a26c
Angel Raimond <142851051+AngelRaimond@users.noreply.github.com>
2026-09-03 23:02:17 -0600

    feat: setup github actions CI/CD (#342)

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Dummy widget test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: Text('Hello World'),
+      ),
+    ));
+
+    expect(find.text('Hello World'), findsOneWidget);
+  });
+}
